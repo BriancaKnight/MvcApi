fdd5abf15d45982405c1772da6491972c75d1726
Brianca Knight <briancaknight@gmail.com>
2024-02-13 16:08:47 -0800

    setup for api calls iin mvc app

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcApiCall.Models;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration; // New using directive.
+
+namespace MvcApiCall.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly string _apikey; // New property.
+
+    // New Controller.
+    public HomeController(IConfiguration configuration)
+    {
+      _apikey = configuration["NYT"];
+    }
+
+    public IActionResult Index()
+    {
+        List<Article> allArticles = Article.GetArticles(_apikey); // Using _apikey here!
+        return View(allArticles);
+    }
+  }
+}
